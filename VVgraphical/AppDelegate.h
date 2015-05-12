1c38cbd78475b6614d0f77e916276fc2a780e3c7
vaibhav <vwebrose@gmail.com>
2015-05-12 11:52:08 +0530

    updated

---
+++ VVgraphical/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VVgraphical
+//
+//  Created by Vaibhav Kumar on 5/12/15.
+//  Copyright (c) 2015 OpenSource. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VVgraphical/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VVgraphical
+//
+//  Created by Vaibhav Kumar on 5/12/15.
+//  Copyright (c) 2015 OpenSource. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
